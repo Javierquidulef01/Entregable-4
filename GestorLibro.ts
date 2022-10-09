309df5c4ff392c78c0b4a02b1991bde92d42114c
irinamarzioni <imarzioni@devartis.com>
2022-10-09 18:09:52 -0300

    correccion

--- GestorLibro.ts
+++ GestorLibro.ts
@@ -1,40 +1,38 @@
 import { Libro } from "./Libro";
 
 export class GestorLibro {
-    private libros:Libro[];
+    private libros: Libro[];
 
-    public constructor () {
+    public constructor() {
         this.libros = [];
     }
 
-    public getLibros():Libro[] {
+    public getLibros(): Libro[] {
         return this.libros;
     }
 
-    public insertar(libro:Libro):boolean{
-        if(libro != null) {
+    public insertar(libro: Libro): boolean {
+        if (libro != null) {
             this.libros.push(libro);
             return true;
         }
         return false;
     }
 
-    public eliminar(codigo:number):boolean {
-        if((this.libros != null) || (codigo > 0)) {
-            for(let i:number = 0; i < this.libros.length; i++) {
+    public eliminar(codigo: number): boolean {
+        if ((this.libros != null) || (codigo > 0)) {
+            for (let i: number = 0; i < this.libros.length; i++) {
                 if (this.libros[i].getCodigo() == codigo) {
-                    this.libros.splice(i,1);
+                    this.libros.splice(i, 1);
                     return true;
                 }
             }
         }
-
         return false;
     }
 
-    
-    public consultar(codigo:number):Libro|null {
-        for(let i:number = 0; i < this.libros.length; i++) {
+    public consultar(codigo: number): Libro | null {
+        for (let i: number = 0; i < this.libros.length; i++) {
             if (this.libros[i].getCodigo() == codigo) {
                 return this.libros[i];
             }
@@ -42,9 +40,8 @@ export class GestorLibro {
         return null;
     }
 
-
-    public actualizar(libro:Libro):boolean {
-        for(let i:number = 0; i < this.libros.length; i++) {
+    public actualizar(libro: Libro): boolean {
+        for (let i: number = 0; i < this.libros.length; i++) {
             // console.log(this.libros[i].getCodigo() + " == " +  libro.getCodigo());
             if (this.libros[i].getCodigo() == libro.getCodigo()) {
                 // console.log("entro al if de actualizar");
@@ -55,6 +52,5 @@ export class GestorLibro {
         }
         return false;
     }
-
 }
 
